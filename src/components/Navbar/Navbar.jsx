2dd5667b95842eb202159b6ccc527943f63794f1
Tran tuan <trantuan9122003@gmail.com>
2024-11-26 10:03:58 +0700

    nocommit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,8 @@ import search_icon from '../../assets/search_icon.svg';
 import bell_icon from '../../assets/bell_icon.svg';
 import profile_img from '../../assets/profile_img.png';
 import caret_icon from '../../assets/caret_icon.svg';
-import {logout} from '../../firebase'
+import { logout } from '../../firebase';
+
 const Navbar = () => {
     const navbarRef = useRef();
 
